500042ff36c9eae979857cfa98300272b157e7f9
KevinMwita7 <kevinmwita7@gmail.com>
2023-06-05 23:22:56 +0000

    Respond to API requests

--- pages/api/tenant/index.ts
+++ pages/api/tenant/index.ts
@@ -1,13 +1,16 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import DbConnection from "@/utils/db";
 import User from "@/model/User";
+import { convertRequestErrorsToJson } from "@/utils";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     await DbConnection.connect();
 
     switch(req.method) {
+        // GET all users
         case "GET":
             return res.send("users");
+        // Create a user
         case "POST":
             try {
                 // Roles should not be assignable from the frontend
@@ -15,8 +18,9 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
                 await User.create(rest);
                 return res.status(200).end();
             } catch(e) {
-                console.log(e);
-                return res.status(400).end();
+                let error = convertRequestErrorsToJson(e);
+                if (error) return res.status(400).json(error);
+                return res.status(500).end();
             }
         default:
             return res.status(405)
--- utils/functions.ts
+++ utils/functions.ts
@@ -1,4 +1,40 @@
+import mongoose from "mongoose";
+
+interface MongoServerError {
+    code: Number,
+    index: Number,
+    keyPattern: {
+        [key: string]: Number
+    },
+    keyValue: {
+        [key: string]: string
+    }
+}
+
 export function addCommas(a: string[]): string {
     if (a.length < 2) return a.join();
     return `${a.slice(0, a.length - 1).join(", ")} or ${a.at(-1)}`;
+}
+
+export function convertRequestErrorsToJson(e: mongoose.Error.ValidationError | MongoServerError /*| mongoose.Error.CastError*/): {} | null {
+    const errors = {};
+    if(e instanceof mongoose.Error.ValidationError) {
+        Object.keys(e.errors).forEach(key => {
+            const error = e.errors[key];
+            errors[key] = {
+                type: error.kind,
+                message: error.message
+            }
+        });
+        return errors;
+    } else if(e.code === 11000) {
+        // Duplicate error
+        let key = Object.keys(e.keyPattern)[0];
+        errors[key] = {
+            type: "custom",
+            message: `Please provide unique ${key}. Received ${e.keyValue[key]}`
+        }
+    }
+    if (Object.keys(errors).length) return errors;
+    return null;
 }
